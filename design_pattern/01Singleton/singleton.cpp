e1665ed3f9c02001107bce43bda228ef8544087f
guolx <guolx@aeroht.com>
2022-02-14 18:12:00 +0800

    add commit for singleton

--- design_pattern/01Singleton/singleton.cpp
+++ design_pattern/01Singleton/singleton.cpp
@@ -2,6 +2,11 @@
 
 //using namespace std;
 
+/*
+    1.某个类只能有一个实例
+    2.它必须自己创建这个实例
+    3.它必须向整个系统提供这个实例
+*/
 class Singleton
 {
 public:
@@ -12,13 +17,13 @@ public:
             sm_instance = new Singleton();
         }
         return sm_instance;
-        
     }
 private:
     Singleton() = default;
     static Singleton * sm_instance;
 };
 
+//init sm_instance as nullptr
 Singleton * Singleton::sm_instance = nullptr;
 
 int main()
